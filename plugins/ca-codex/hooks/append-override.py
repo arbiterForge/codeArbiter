9765bda9a143814692660bffc3b439796c4f571f
codeArbiter release <release@codearbiter.invalid>
2000-01-01 00:00:00 +0000

    Promote ca-codex 0.13.12 qualified distribution

--- plugins/ca-codex/hooks/append-override.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-"""CLI entry point for the locked mandatory override audit append."""
-
-from _overrideappendlib import main
-
-
-if __name__ == "__main__":
-    raise SystemExit(main())
--- plugins/ca-codex/hooks/resolve-append-only-conflict.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-# codeArbiter - sanctioned helper entry point for H-05 append-only conflicts.
-
-from _appendonlyconflictlib import main
-
-
-if __name__ == "__main__":
-    raise SystemExit(main())
